508fed23b6434187f65842d355eaa80f0f681ef1
yaodix <liuyao199111@163.com>
2022-10-08 23:22:36 +0800

    enhance helloworld demo

--- 2_ros_bacsic/src/helloworld_pkg/src/demo.cc
+++ 2_ros_bacsic/src/helloworld_pkg/src/demo.cc
@@ -1,12 +1,17 @@
 #include "ros/ros.h"
 
 int main(int argc, char *argv[]) {
+
+    // 解决中文乱码
+    setlocale(LC_ALL, "");
+    
     // 执行 ros 节点初始化
     ros::init(argc,argv,"hello");
     // 创建 ros 节点句柄(非必须)
     ros::NodeHandle n;
     // 控制台输出 hello world
-    ROS_INFO("hello world!");
+    ROS_INFO("hello world!哈哈");
+    
 
     return 0;
 }
